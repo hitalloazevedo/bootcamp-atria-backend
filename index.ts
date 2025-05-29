3cdc7889204a07903a2c72e7bd2c83c0e491f550
Richard Miller de Sousa <richardmillerdesousa@gmail.com>
2025-05-29 08:12:23 -0300

    Configura autenticação JWT, implementa database inMemoryDB (com pendências)

--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
-import express from "express"
-import publicRoutes from './src/routes/public.ts'
-import privateRoutes from './src/routes/private.ts'
-import auth from auth.ts
+import express from "express";
+import publicRoutes from './src/routes/public.ts';
+import privateRoutes from './src/routes/private.ts';
+import ensureAuthenticated from './src/middlewares/ensureAuthenticated'; // middleware
 
-// varios erros estão presentes pois varias implementações serão feitas 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
-
-app.use('/', publicRoutes)
-app.use('/', auth, privateRoutes)
-
+app.use('/', publicRoutes);
+app.use('/tasks', ensureAuthenticated, privateRoutes);
 
 app.listen(3000, () => {
-    console.log("Servidor rodando em localhost:3000")
-})
+    console.log("Servidor rodando em localhost:3000");
+});
